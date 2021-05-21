8d6c5ddb7de19825f7ae254abf1caf836c832fa3
wcgzgj <zhangjia_good@126.com>
2021-05-21 16:43:54 +0800

    更新readme

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
   </div>
 </template>
 
@@ -12,7 +11,7 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+
   },
 });
 </script>
